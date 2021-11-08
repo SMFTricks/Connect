97c741c2de1592b52ede53768d3847c9c5400a1c
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-07 20:09:05 -0600

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,32 @@
+<?php
+// Version: 2.0; ThemeStrings
+
+$txt['icons_check'] = 'Enable social icons';
+$txt['rs_facebook'] = 'Facebook';
+$txt['facebook_check'] = 'Enable icon of Facebook';
+$txt['facebook_text'] = 'URL of the page to Facebook';
+$txt['rs_twitter'] = 'Twitter';
+$txt['twitter_check'] = 'Enable icon of Twitter';
+$txt['twitter_text'] = 'URL of the page to Twitter';
+$txt['rs_youtube'] = 'Youtube';
+$txt['youtube_check'] = 'Enable icon of Youtube';
+$txt['youtube_text'] = 'URL of the page to Youtube';
+$txt['rs_rss'] = 'RSS';
+$txt['rss_check'] = 'Enable icon of RSS';
+$txt['rss_text'] = 'URL of the page to RSS';
+$txt['cm_link'] = 'Link 1';
+$txt['cm_text'] = 'Text 1';
+$txt['cm_link2'] = 'Link 2';
+$txt['cm_text2'] = 'Text 2';
+$txt['cm_link3'] = 'Link 3';
+$txt['cm_text3'] = 'Text 3';
+$txt['cm_link4'] = 'Link 4';
+$txt['cm_text4'] = 'Text 4';
+$txt['cm_link5'] = 'Link 5';
+$txt['cm_text5'] = 'Text 5';
+$txt['cm_mu1'] = 'My profile';
+$txt['cm_mu2'] = 'Edit profile';
+$txt['cm_mu3'] = 'New replies';
+$txt['cm_mu4'] = 'New posts';
+
+?>
---
+++ languages/ThemeStrings.spanish_es.php
@@ -0,0 +1,32 @@
+<?php
+// Version: 2.0; ThemeStrings
+
+$txt['icons_check'] = 'Activar iconos sociales';
+$txt['rs_facebook'] = 'Facebook';
+$txt['facebook_check'] = 'Activar icono de Facebook';
+$txt['facebook_text'] = 'Direccion de la pagina de Facebook';
+$txt['rs_twitter'] = 'Twitter';
+$txt['twitter_check'] = 'Activar icono de Twitter';
+$txt['twitter_text'] = 'Direccion de la pagina de Twitter';
+$txt['rs_youtube'] = 'Youtube';
+$txt['youtube_check'] = 'Activar icono de Youtube';
+$txt['youtube_text'] = 'Direccion de la pagina de Youtube';
+$txt['rs_rss'] = 'RSS';
+$txt['rss_check'] = 'Activar icono de RSS';
+$txt['rss_text'] = 'Direccion de la pagina de RSS';
+$txt['cm_link'] = 'Link 1';
+$txt['cm_text'] = 'Text 1';
+$txt['cm_link2'] = 'Link 2';
+$txt['cm_text2'] = 'Text 2';
+$txt['cm_link3'] = 'Link 3';
+$txt['cm_text3'] = 'Text 3';
+$txt['cm_link4'] = 'Link 4';
+$txt['cm_text4'] = 'Text 4';
+$txt['cm_link5'] = 'Link 5';
+$txt['cm_text5'] = 'Text 5';
+$txt['cm_mu1'] = 'Mi perfil';
+$txt['cm_mu2'] = 'Editar perfil';
+$txt['cm_mu3'] = 'Nuevas respuestas';
+$txt['cm_mu4'] = 'Nuevos temas';
+
+?>
